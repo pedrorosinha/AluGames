20fae7ded35b95b77cb95a7a18750cc5fe9cffc0
pedro felipe <pedrofelipinho@gmail.com>
2025-09-12 14:23:19 -0300

    Testando a api de jogos

--- src/main/kotlin/br/com/alura/alugames/principal/TesteJson.kt
+++ src/main/kotlin/br/com/alura/alugames/principal/TesteJson.kt
@@ -5,6 +5,8 @@ import br.com.alura.alugames.services.ConsumoApi
 fun main() {
     val consumoApi = ConsumoApi()
     val listaGamers = consumoApi.buscaGamers()
+    val jogoApi  = consumoApi.buscaJogo("150")
 
     println(listaGamers)
+    println(jogoApi)
 }
